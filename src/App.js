3474e9231ff08ce906f922f0d02929ed82ff6d73
keneishac <keneishacorbett@gmail.com>
2021-04-13 13:56:07 -0700

    adds material ui styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import {Typography, AppBar } from '@material-ui/core'
 
 const App = () => {
     return (
-        <div>
-            Video Chat App
-        </div>
+        <AppBar positon='static' color='inherit'>
+          <Typography variant='h2' align='center'>Let's Chat</Typography>
+          {/* Video Player */}
+          {/* Options -> Notifications */}
+        </AppBar>
     )
 }
 
