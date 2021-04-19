0a31e9ad18c1b42a3248761ea60de1c5423e881e
keneishac <keneishacorbett@gmail.com>
2021-04-19 14:49:55 -0700

    adds stying

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,6 +1,29 @@
 import React from 'react'
+import { Grid, Typography, Paper} from '@material-ui/core'
+import { makeStyles} from '@material-ui/core/styles'
+import {SocketContext } from '../SocketContext'
 
-const VideoPlayer = () => {
+const useStyles = makeStyles((theme) => ({
+    video: {
+      width: '550px',
+      [theme.breakpoints.down('xs')]: {
+        width: '300px',
+      },
+    },
+    gridContainer: {
+      justifyContent: 'center',
+      [theme.breakpoints.down('xs')]: {
+        flexDirection: 'column',
+      },
+    },
+    paper: {
+      padding: '10px',
+      border: '2px solid black',
+      margin: '10px',
+    },
+  }));
+
+const VideoPlayer = (   ) => {
     return (
         <div>
             VideoPlayer
