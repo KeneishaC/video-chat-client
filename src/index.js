c9ad3dbafeaaa13b4e08c0b6de03d0217a2a9120
keneishac <keneishacorbett@gmail.com>
2021-04-19 14:46:21 -0700

    imports context provider into index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {ContextProvider } from './SocketContext'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+   <ContextProvider>
+     <App />
+   </ContextProvider>,
+   
   document.getElementById('root')
 );
 
