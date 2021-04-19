13653c93f1bbf41f26fbaea48243993f744bd863
keneishac <keneishacorbett@gmail.com>
2021-04-19 15:04:25 -0700

    fixes  typo in imports

--- src/SocketContext.js
+++ src/SocketContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState, useRef, useEffect} from 'react'
+import React, { createContext, useState, useRef, useEffect} from 'react'
 import { io } from 'socket.io-client'
 import Peer from 'simple-peer'
 
