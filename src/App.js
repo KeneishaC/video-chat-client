c50bf735ab3964cba04222059867576809ae925f
keneishac <keneishacorbett@gmail.com>
2021-04-19 15:03:47 -0700

    fixes import typo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Typography, AppBar } from '@material-ui/core'
-import {makeStyles } from '@material-ui/core'
+import {makeStyles } from '@material-ui/core/styles'
 import VideoPlayer from './components/VideoPlayer'
 import Options from './components/Options'
 import Notifications from './components/Notifications'
