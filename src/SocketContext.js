4d4c005ef945e7b9ffac3dc826f9ca05a7d3bfd0
keneishac <keneishacorbett@gmail.com>
2021-04-19 13:56:56 -0700

    completes answer call functionthat allows for answerc call from each user

--- src/SocketContext.js
+++ src/SocketContext.js
@@ -9,9 +9,16 @@ const socket = io ('http://localhost:3001')
 const ContextProvider = ( {children}) => {
     const [stream, setStream] = useState(null)
     const [me, setMe] = useState('')
-    const  [call, setCall] = useState({})
+    const [call, setCall] = useState({})
+    const [callAccepted, setCallAccepted] = useState(false)
+    const [callEnded, setCallEnded] = useState(false)
+
 
     const myVideo = useRef()
+    const userVideo = useRef()
+    const connectionRef = useRef()
+
+
 
     useEffect(() => {
         navigator.medisDevices.getUserMedia({ video: true,  audio: true})
@@ -29,7 +36,22 @@ const ContextProvider = ( {children}) => {
      }, [])
 
     const answerCall = () => {
+        setCallAccepted(true)
 
+        const peer = new Peer({ initiator: false, trickle: false, stream })
+
+        peer.on('signal', (data) => {
+            socket.emit('answercall', { signael: data, to: call.from})
+        })
+
+        //other users strem
+        peer.on('stream', (currentStream) => {
+            userVideo.current.srcObject = currentStream
+        })
+
+        peer.signal(call.signal)
+
+        connectionRef.current = peer 
     }
 
     const callUser = () => {
@@ -41,3 +63,4 @@ const ContextProvider = ( {children}) => {
     }
 }
 
+export default ContextProvider 
