7fc9b1c517a3255979c3ca64945663d3f28f4354
keneishac <keneishacorbett@gmail.com>
2021-04-23 17:44:22 -0700

    adds styling

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,5 +1,42 @@
 import React from 'react'
+import { Button, TextField, Grid, Tyopgraphy, Container, Paper } from '@material-ui/core'
+import { makeStyles} from "@material-ui/core/styles"
+import { CopyToClipBoard } from 'react-copy-to-clipboard'
+import { Assignment, Phone, PhoneDisabled } from "@material-ui/icons"
 
+import { SocketContext } from '../SocketContext'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    gridContainer: {
+      width: '100%',
+      [theme.breakpoints.down('xs')]: {
+        flexDirection: 'column',
+      },
+    },
+    container: {
+      width: '600px',
+      margin: '35px 0',
+      padding: 0,
+      [theme.breakpoints.down('xs')]: {
+        width: '80%',
+      },
+    },
+    margin: {
+      marginTop: 20,
+    },
+    padding: {
+      padding: 20,
+    },
+    paper: {
+      padding: '10px 20px',
+      border: '2px solid black',
+    },
+  }));
+ 
 const Options = (  { children } ) => {
     return (
         <div>
