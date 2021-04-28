5c1e0a80d03c7c14cb3f7e803d1b9d3c89340865
keneishac <keneishacorbett@gmail.com>
2021-04-27 18:03:11 -0700

    removes typos

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -4,18 +4,19 @@ import { SocketContext } from '../SocketContext'
 
 const Notifications = () => {
     const { answerCall, call, callAccepted } = useContext(SocketContext)
+
     return (
         <>
-           { call.isReceivedCall && !callAccepted && (
-               <div style={{ display: 'flex', justifyContent: 'center' }}>
-                   <h1>{call.name} is calling</h1>
-                   <Button variant="contained" color="primary" onClick={answerCall}>
-                        Answer
-                   </Button>
-               </div>
-           )}
+        {call.isReceivingCall && !callAccepted && (
+            <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+            <h1>{call.name} is calling:</h1>
+            <Button variant="contained" color="primary" onClick={answerCall}>
+                Answer
+            </Button>
+            </div>
+        )}
         </>
-    )
+  )
 }
 
 export default Notifications
