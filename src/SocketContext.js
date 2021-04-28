325a72568eb45b23abed7d2bdb76028a4b8de742
keneishac <keneishacorbett@gmail.com>
2021-04-27 18:33:28 -0700

    prepares app for live deploy of backend of app

--- src/SocketContext.js
+++ src/SocketContext.js
@@ -5,6 +5,8 @@ import Peer from 'simple-peer'
 const SocketContext = createContext()
 
 const socket = io ('http://localhost:3001')
+// const socket = io ('')
+
 
 const ContextProvider = ( {children}) => {
     const [callAccepted, setCallAccepted] = useState(false)
